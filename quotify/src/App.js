e7b36350c0a339b0c7c8e90f340d3728e2d51f5a
SheebaJ <ashasheebajnn1996@gmail.com>
2020-02-27 16:51:52 +0530

    javascript

--- quotify/src/App.js
+++ quotify/src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Get_Quotes from './Get_Quotes.js';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+        <Link to="/">Home</Link><br/>
+        <Link to="/quotes">Get Quotes</Link><br/>
+        {/* <Link to="/quotes/:id">View Quote</Link><br/>  */}
+    <Switch>
+        <Route exact path="/"><Home/></Route>
+      <Route path="/quotes"><Get_Quotes/></Route>
+    </Switch>
+    </Router>
   );
 }
 
-export default App;
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      s:""
+    };
+  }
+  handleClick() {
+      // console.log('i am caled');
+      axios.get(`https://jsonplaceholder.typicode.com/posts`)
+        .then(res => {
+        console.log(res.data);
+          const persons = res.data;
+          let x=JSON.stringify(persons);
+     localStorage.setItem('posts',JSON.stringify(persons));
+          var result = JSON.parse(x);
+          var s = result.length;
+          console.log(result.length);
+          this.setState({s:s});
+        })
+    }
+
+    render() {
+      return (
+        <div><br/>
+          <label>Total Record</label>{this.state.s}<br/>
+          <button onClick={this.handleClick}> Fetch Quotes</button>    
+              </div>
+      )
+    }
+  }
---
+++ quotify/src/Get_Quotes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Get_Quotes extends React.Component {
+    render() {
+        return (
+  
+          <ul>
+            {"Hello"}
+          </ul>
+        )
+      }
+    }
+
+export default Get_Quotes;
